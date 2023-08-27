0092e45d995296bd9b0148b3005de84af849921d
praticodes <pratibha.thakur@mail.utoronto.ca>
2023-08-27 17:45:30 +0200

    loading

--- app.py
+++ app.py
@@ -17,12 +17,13 @@ def launch():
 @app.route('/submit', methods=['POST'])
 def submit():
     text = request.form['text']
+    show_loading_overlay = True
     result = analysis.get_compellingness_ai(text)
     descriptions = analysis.get_compellingness_description(result)
     warnings = analysis.ethics_warning(text)
     pathos = analysis.get_pathos_description(result)
     logos = analysis.get_logos_description(result)
-    return render_template('results.html', text=text, result=result, descriptions=descriptions, warnings=warnings, pathos=pathos, logos=logos)
+    return render_template('results.html', text=text, result=result, descriptions=descriptions, warnings=warnings, pathos=pathos, logos=logos, show_loading_overlay=show_loading_overlay)
 
 
 if __name__ == '__main__':
